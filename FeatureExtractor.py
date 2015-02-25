ac54d97d6d7dec99529e8929c94e75216aea50b4
Heba Aamer <heba93aamer@gmail.com>
2015-02-25 19:06:52 +0200

    pulling master

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -8,11 +8,11 @@ class FeatureExtractor:
 
   def extract_feature_vector(self):
     img = cv.imread(self.image.get_path())
-    img_resized = cv.resize(img, (300, 250)) #resizing the image
+    img_resized = cv.resize(img, (300, 250)) #resizing the image (those values should be in the configurations)
     gray = cv.cvtColor(img_resized, cv.COLOR_BGR2GRAY)
     gray_equalized = cv.equalizeHist(gray) #equalizing the histogram (must take a gray scale image)
     sift = cv.SIFT()
-    kp, des = sift.detectAndCompute(gray2, None)
+    kp, des = sift.detectAndCompute(gray_equalized, None)
     return des
     
 
