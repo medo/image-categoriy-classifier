9295a0886368df286c70234f8406ed76c2ffff55
Heba Aamer <heba93aamer@gmail.com>
2015-02-25 19:01:23 +0200

    removing CLAHE

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -11,8 +11,6 @@ class FeatureExtractor:
     img_resized = cv.resize(img, (300, 250)) #resizing the image
     gray = cv.cvtColor(img_resized, cv.COLOR_BGR2GRAY)
     gray_equalized = cv.equalizeHist(gray) #equalizing the histogram (must take a gray scale image)
-#     clahe = cv.createCLAHE(tileGridSize=(10,10))
-#     gray2 = clahe.apply(gray)
     sift = cv.SIFT()
     kp, des = sift.detectAndCompute(gray2, None)
     return des
